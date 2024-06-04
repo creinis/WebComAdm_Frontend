35baffe69a40c4f4fbc4ea81cc4729d1d45347e2
creinis <115181527+creinis@users.noreply.github.com>
2024-06-04 21:31:22 +0200

    tryout API_URL out login

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`;
+/* const API_URL = `${import.meta.env.VITE_API_URL}/api/auth`; */
 
 export const login = async (email, password) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
+  const response = await axios.post(`/login`, { email, password });
   return response.data;
 };
 
 export const register = async (adminName, email, password, role) => {
-  const response = await axios.post(`${API_URL}/register`, { adminName, email, password, role });
+  const response = await axios.post(`/register`, { adminName, email, password, role });
   return response.data;
 };
